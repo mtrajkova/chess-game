0d07a6544af5d3ad067419ed961203b69d3062f0
SlavcheGj <slavce_gorgiev@hotmail.com>
2019-08-20 09:44:32 +0200

    Game controller get game by id

---
+++ src/main/java/com/project/chess/Conterollers/GameController.java
@@ -0,0 +1,34 @@
+package com.project.chess.Conterollers;
+
+import com.project.chess.model.Game;
+import com.project.chess.service.GameService;
+import com.project.chess.service.impl.GameServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.PublicKey;
+
+
+@RestController
+@RequestMapping("/")
+public class GameController {
+
+    /*@Autowired*/
+    private GameService gameService;
+
+    @Autowired
+    public GameController(GameServiceImpl gameService) {
+
+        this.gameService = gameService;
+
+    }
+
+    @GetMapping(value = "/game/{id}")
+    public ResponseEntity<?> getGameById(@PathVariable(value = "id") Long id) {
+        Game returnedGame = gameService.getGameById(id);
+        return new ResponseEntity<>(returnedGame, HttpStatus.OK);
+    }
+
+}
