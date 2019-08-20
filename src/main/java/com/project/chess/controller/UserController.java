5d3fe4117a62decd8a572ba18ded437226f0409b
Kristijan Nastevski <nastevski_kristijan@yahoo.com>
2019-08-20 12:06:06 +0200

    Modify UserController

--- src/main/java/com/project/chess/controller/UserController.java
+++ src/main/java/com/project/chess/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.project.chess.controller;
 
 
+import com.project.chess.model.Users;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 import com.project.chess.service.UserService;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @CrossOrigin(origins = "*")
@@ -19,6 +26,22 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody Users user, BindingResult bindingResult) {
+
+        if(bindingResult.hasErrors()) {
+            List<FieldError> errors = bindingResult.getFieldErrors();
+            for (FieldError error : errors ) {
+                System.out.println (error.getObjectName() + " - " + error.getDefaultMessage());
+            }
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+
+        Users createdUser = userService.createUser(user);
+        return new ResponseEntity<Users>(createdUser, HttpStatus.CREATED);
     }
 
 }
