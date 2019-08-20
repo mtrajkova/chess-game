522f17dc25a523f47bd0c3f09d9cd734c76fecfd
Stefanija Kralevska <Stefanija.Kralevska@endava.com>
2019-08-20 11:42:59 +0200

    Update for users entity

--- src/main/java/com/project/chess/model/Users.java
+++ src/main/java/com/project/chess/model/Users.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.experimental.Wither;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import java.util.List;
 
 @Wither
@@ -14,7 +15,16 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Email
+    @Column(unique = true)
     private String username;
+
+    @Column(unique = true)
+    private String display_name;
+
+    private String password;
+
+    private boolean logged_in;
 
     @OneToMany(mappedBy = "playerOne")
     private List<Game> gamesPlayerOne;
@@ -25,8 +35,11 @@ public class Users {
     public Users() {
     }
 
-    public Users(String username) {
-        this.username = username;
+    public Users(@Email String email, String display_name, String password, boolean status) {
+        this.username = email;
+        this.display_name = display_name;
+        this.password = password;
+        this.logged_in = status;
     }
 
     public Long getId() {
@@ -43,5 +56,29 @@ public class Users {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public String getDisplay_name() {
+        return display_name;
+    }
+
+    public void setDisplay_name(String display_name) {
+        this.display_name = display_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLogged_in() {
+        return logged_in;
+    }
+
+    public void setLogged_in(boolean logged_in) {
+        this.logged_in = logged_in;
     }
 }
