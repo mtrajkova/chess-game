e626fa07f3b0d4e5ca20df57622511efba509919
Kristijan Nastevski <nastevski_kristijan@yahoo.com>
2019-08-20 10:41:10 +0200

    Adding UserController

--- src/main/java/com/project/chess/controller/UserController.java
+++ src/main/java/com/project/chess/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.project.chess.controller;
 
 
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.project.chess.service.UserService;
 
 
 @CrossOrigin(origins = "*")
@@ -11,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
 }
