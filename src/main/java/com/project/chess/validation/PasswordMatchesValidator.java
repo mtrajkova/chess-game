91f6c52eeeb6594a5f8bcc6524cad1df161cb5a5
Stefanija Kralevska <Stefanija.Kralevska@endava.com>
2019-08-21 09:12:10 +0200

    Add password matches validation

--- src/main/java/com/project/chess/validation/PasswordMatchesValidator.java
+++ src/main/java/com/project/chess/validation/PasswordMatchesValidator.java
@@ -1,4 +1,20 @@
 package com.project.chess.validation;
 
-public class PasswordMatchesValidator {
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.project.chess.model.dto.UsersDto;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+   @Override
+   public void initialize(final PasswordMatches constraintAnnotation) {
+   }
+
+   @Override
+   public boolean isValid(final Object obj, final ConstraintValidatorContext context) {
+      final UsersDto user = (UsersDto) obj;
+      return user.getPassword().equals(user.getMatchingPassword());
+   }
+
 }
