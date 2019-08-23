2240ef2fd35c50bf1a4f2f65136c029326a3bc74
slimeaseason <ntomikj@gmail.com>
2019-08-23 15:29:13 +0200

    Improved PlayGameServiceImpl

--- src/main/java/com/project/chess/service/impl/PlayGameServiceImpl.java
+++ src/main/java/com/project/chess/service/impl/PlayGameServiceImpl.java
@@ -1,11 +1,14 @@
 package com.project.chess.service.impl;
 
 import com.github.bhlangonijr.chesslib.Board;
-import com.github.bhlangonijr.chesslib.move.MoveGenerator;
-import com.github.bhlangonijr.chesslib.move.MoveGeneratorException;
+import com.github.bhlangonijr.chesslib.Piece;
+import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.*;
 import com.project.chess.exception.GameNotFoundException;
 import com.project.chess.model.Game;
+import com.project.chess.model.State;
 import com.project.chess.model.Status;
+import com.project.chess.model.dto.MoveDto;
 import com.project.chess.model.dto.MoveResponseDto;
 import com.project.chess.repository.GameRepository;
 import com.project.chess.service.PlayGameService;
@@ -30,10 +33,11 @@ public class PlayGameServiceImpl implements PlayGameService {
     }
 
     private MoveResponseDto getMoveResponseFromGame(Game game) throws MoveGeneratorException {
+
         Board board = getCurrentBoard(game);
 
         MoveResponseDto moveResponseDto = new MoveResponseDto();
-        moveResponseDto.setFEN(board.getFen());
+        moveResponseDto.setFEN(game.getLastState().getFEN());
         moveResponseDto.setPGN(game.getPGN());
         moveResponseDto.setLegalMoves(MoveGenerator.generateLegalMoves(board));
 
@@ -41,11 +45,43 @@ public class PlayGameServiceImpl implements PlayGameService {
     }
 
     private Board getCurrentBoard(Game game) {
+
         String fen = game.getLastState().getFEN();
         Board board = new Board();
         board.loadFromFen(fen);
 
         return board;
     }
+
+    @Override
+    public MoveResponseDto getMoveResponseOnMove(Game game, MoveDto moveDto) throws MoveGeneratorException, MoveConversionException {
+
+        Move move = new Move(Square.fromValue(moveDto.getFrom().toUpperCase()), Square.fromValue(moveDto.getTo().toUpperCase()), Piece.fromValue(moveDto.getPromotion().toUpperCase()));
+
+        validateAndUpdateGame(game, move);
+
+        return getMoveResponseFromGame(game);
+    }
+
+    private void validateAndUpdateGame(Game game, Move move) throws MoveGeneratorException, MoveConversionException {
+
+        Board board = getCurrentBoard(game);
+
+        if (MoveGenerator.generateLegalMoves(board).contains(move)) {
+            board.doMove(move);
+
+            game.setLastState(new State(board.getFen()));
+            game.setPGN(updatePgnOnMove(game, move));
+        }
+    }
+
+    private String updatePgnOnMove(Game game, Move move) throws MoveConversionException {
+
+        MoveList movesHistoryPgn = new MoveList();
+        movesHistoryPgn.loadFromSan(game.getPGN());
+        movesHistoryPgn.add(move);
+        return movesHistoryPgn.toSan();
+    }
+
 
 }
