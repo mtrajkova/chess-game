9f998110c34ddded1035527de25751bad24beaaf
vsrbinoski <viktor.srbinoski@endava.com>
2019-08-23 12:51:37 +0200

    Refactor SecurityConstants
    Now they use @Value to inject values from application.properties

--- src/main/java/com/project/chess/configuration/security/SecurityConstants.java
+++ src/main/java/com/project/chess/configuration/security/SecurityConstants.java
@@ -1,12 +1,36 @@
 package com.project.chess.configuration.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class SecurityConstants {
     public static final String LOGIN_URL = "/user/login";
     public static final String REGISTER_URL = "/user/registration";
 
     //  https://www.allkeysgenerator.com/ is used to generate the secret key. Options - Encryption key - 512-bit
-    public static final String SECRET = "C*F-JaNdRgUjXn2r5u8x/A?D(G+KbPeShVmYp3s6v9y$B&E)H@McQfTjWnZr4t7w";
-    public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_STRING = "Authorization";
-    public static final long EXPIRATION_TIME = 864_000_000;
+    public static String SECRET;
+    public static String TOKEN_PREFIX;
+    public static String HEADER_STRING;
+    public static long EXPIRATION_TIME;
+
+    @Value("${jwt.secret}")
+    public void setSECRET(String SECRET) {
+        SecurityConstants.SECRET = SECRET;
+    }
+
+    @Value("${jwt.tokenPrefix}")
+    public void setTokenPrefix(String tokenPrefix) {
+        TOKEN_PREFIX = tokenPrefix;
+    }
+
+    @Value("${jwt.header}")
+    public void setHeaderString(String headerString) {
+        HEADER_STRING = headerString;
+    }
+
+    @Value("${jwt.expirationTime}")
+    public void setExpirationTime(long expirationTime) {
+        EXPIRATION_TIME = expirationTime;
+    }
 }
